d5678469b1d82a1fa8dcb5384b9710e57ea72e2d
Dusan Milko <dusan.milko@rokkan.com>
2015-04-01 13:25:24 -0400

    remove foundation and other non generic info, add icons, better readme, docs, helpers and more

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,6 +12,9 @@ var del = require('del');
 var cssGlobbing = require('gulp-css-globbing');
 var todo = require('gulp-todo');
 var assemble = require('assemble');
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var lodash = require('lodash');
 
 var paths = {
   images: 'src/assets/imgs/**/*'
@@ -25,20 +28,56 @@ gulp.task('clean', function(cb) {
 
 // ----------------------------------------------------------------
 
+// Generate pages and docs
 assemble.data(['src/data/**/*.{json,yml}']);
+assemble.helpers('src/helpers/**/*.js');
 assemble.partials('src/views/partials/**/*.hbs');
 assemble.layouts('src/views/layouts/**/*.hbs');
 
-gulp.task('assemble', function() {
+assemble.task('docs', function() {
+  assemble.src('src/views/docs/**/*.hbs')
+    .pipe(rename(function (path) {
+      path.extname = ".html"
+    }))
+    .pipe(assemble.dest('build/docs/'));
+});   
+assemble.task('pages', function() {
   assemble.src('src/views/pages/**/*.hbs')
     .pipe(rename(function (path) {
       path.extname = ".html"
     }))
     .pipe(assemble.dest('build/'));
 });
+gulp.task('assemble', function() {
+  assemble.run('docs');
+  assemble.run('pages');
+});
 
 // ----------------------------------------------------------------
 
+// Generate Icon font form svgs 
+gulp.task('iconfont', function(){
+  gulp.src(['src/assets/icons/svgs/*.svg'])
+    .pipe(iconfont({ fontName: 'custom-icon-font' }))
+    .on('codepoints', function(codepoints, options) {
+      gulp.src('src/assets/icons/_icon-font.css')
+        .pipe(consolidate('lodash', {
+          glyphs: codepoints,
+          fontName: 'custom-icon-font',
+          fontPath: '../fonts/icons/',
+          className: 'i'
+        }))
+        .pipe(rename(function (path) {
+          path.extname = ".scss"
+        }))
+        .pipe(gulp.dest('src/assets/scss'));
+    })
+    .pipe(gulp.dest('src/assets/fonts/icons'));
+});
+
+// ----------------------------------------------------------------
+
+// Generate css from scss
 gulp.task('css', function() {
   gulp.src('src/assets/scss/main.scss')
     .pipe(cssGlobbing({
@@ -56,28 +95,42 @@ gulp.task('css', function() {
       }
     }))
     .pipe(sass())
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('ERROR: ')+gutil.colors.yellow(err.message));
+      this.emit('end');
+    })
     .pipe(autoprefixer('last 5 versions'))
     //.pipe(minifycss())
     .pipe(gulp.dest('build/assets/css'))
-    .pipe(notify({ message : 'Gulp Complete'}));
+    .pipe(notify({ message : 'Gulp CSS Complete'}));
 });
 
+// ----------------------------------------------------------------
+
+// Generate JS with browserify with sourcemaps
 gulp.task('js', function() {
   gulp.src('src/assets/js/libs/**/*')
     .pipe(gulp.dest('build/assets/js/libs'))
   gulp.src('src/assets/js/main.js')
     .pipe(browserify({ debug : true }))
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('ERROR: ')+gutil.colors.yellow(err.message));
+      this.emit('end');
+    })
     .pipe(gulp.dest('build/assets/js'))
-    .pipe(notify({ message : 'Gulp Complete'}));
+    .pipe(notify({ message : 'Gulp JS Complete'}));
 });
 
+// Compress js
 gulp.task('compressjs', function() {
   gulp.src('build/assets/js/main.js')
     .pipe(uglify())
     .pipe(rename('min.js'))
     .pipe(gulp.dest('build/assets/js'))
-    .pipe(notify({ message : 'Gulp Complete'}));
+    .pipe(notify({ message : 'Gulp Compress JS Complete'}));
 });
+
+// ----------------------------------------------------------------
 
 // Copy all static images
 gulp.task('images', function() {
@@ -86,9 +139,10 @@ gulp.task('images', function() {
     .pipe(gulp.dest('build/assets/imgs'));
 });
 
+// Copy all fonts
 gulp.task('fonts', function() {
-  return gulp.src('src/assets/fonts')
-    .pipe(gulp.dest('build/assets/'));
+  return gulp.src('src/assets/fonts/**/*')
+    .pipe(gulp.dest('build/assets/fonts'));
 });
 
 // ----------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Static Builder
 
-A light scaffold to build static sites using assemble, handlebars, gulp, browserify, sass, json.
+A light scaffold to build static sites using assemble, gulp, browserify, sass, handlebars, and json.
+
+All the files you edit go into **src** and get compiles into **build**
+
+## Setup
+
+NPM is required to be installed.
+
+Run `npm install` after clone to download gulp and other required modules.
 
 
 ### Tasks:
@@ -15,11 +23,13 @@ A light scaffold to build static sites using assemble, handlebars, gulp, browser
 
 **gulp assemble** - generate all pages
 
-**gulp fonts** - cmove fonts to build folder
+**gulp fonts** - move fonts to build folder
 
 **gulp todo** - go through and generate todo's based off of comments "//TODO:"
 
 **gulp watch** - look for changes in src/pages|scss|js
+
+**gulp iconfont** - generate icon font
 
 
 ### Folder Structure
@@ -28,20 +38,30 @@ A light scaffold to build static sites using assemble, handlebars, gulp, browser
 └── src
   ├── assets
   │    ├── fonts
+  │    ├── icons
+  │    │   ├── _icon-font.css (icon font template)
+  │    │   └── svgs
+  │    │        └── *.svg
   │    ├── imgs
   │    │   └── *.*
   │    ├── js
   │    │   ├── libs
   │    │   ├── app
+  │    │   ├── templates
   │    │   └── main.js
   │    └── scss
   │        ├── libs
   │        ├── partials
+  │        ├── elements
   │        ├── main.scss
   │        └── _*.scss
   ├── data
   │   └── *.{json,yml}
+  ├── helpers
+  │   └── *.js
   └── views
+      ├── docs
+      │   └── *.{hbs,html,php}
       ├── partials
       │   └── *.{hbs,html,php}
       ├── layouts
@@ -50,31 +70,75 @@ A light scaffold to build static sites using assemble, handlebars, gulp, browser
           └── index.{hbs,html,php}
 ```
 
-## Markup
+### Pages
 
-Declare array 
+All the files in **pages** folder are used to generate static pages.
+These files in pages should contain the core content of the page.
+The layout files will contain the major layout and are refrenced to by front matter(more on that below)
+Pages can also load partials anywhere in the page
 
+
+##### Front matter
+
+Pages can use YAML front matter as shown below or call a variable from a data.json file in the **data** folder, for things like the layout.
+
+```
 ---
+layout: 'default'
+title: 'site'
 arr:
 - {"a":"b"}
+---
 
 this.arr.[0] // {"a","b"}
 
 ---
+```
 
+##### Loading partial
 
-Call Partial
-
+```
 {{>nav }}
 
-Pass current page variable into partial
-
+with current front matter
 {{>nav this}}
 
-Pass global data variable into partial 
+with custom variable
+{{>nav key="value"}}
 
+with global data variable
 {{>nav site}}
 
-Pass inline variable
+with custom json
+{{#parseJSON '{
+  "a": "b",
+  "c": "d"
+}'}}
+{{> nav }}
+{{/parseJSON}}
 
-{{>nav key="value"}}
+```
+
+### SCSS
+
+All scss goes into the "src/assets/scss/" 
+
+All scss goes into the "src/assets/scss/libs"  
+
+### JS
+
+Using Browserify for JS
+
+All js goes into the "src/assets/js/app"
+Refrence js in the main.js file located in "src/assets/js/" 
+
+All libraries goes into the "src/assets/js/libs"  
+
+If you do any js templating put that into "src/assets/js/templates/"
+
+### Icon Font
+
+Put svg files into "src/assets/icons" 
+Then run **gulp iconfont** to generate font and matching scss file
+
+
